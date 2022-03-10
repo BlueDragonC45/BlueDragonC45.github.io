25878f0c6f401f774f8eafe6584014498698c337
BlueDragonC45 <cooldragondjc@gmail.com>
2022-03-10 16:35:32 -0500

    Finished most forms, Testing cookie login

--- CSI2132/scripts/main.js
+++ CSI2132/scripts/main.js
@@ -1,18 +1,124 @@
 
-function openPane(tabName) {
-  console.log("Here");
+function openPane(paneName) {
+	validated = validate(paneName);
+	switch(paneName) {
+		case 'receptionists':
+			if (validated) {
+				document.getElementById("receptionistNav").style.display = "block";
+			} else {
+				document.getElementById("validateRec").style.display = "block";
+			}
+			break;
+		case 'dentists':
+			if (validated) {
+				document.getElementById("dentistNav").style.display = "block";
+			} else {
+				document.getElementById("validateDen").style.display = "block";
+			}
+			break;
+		case 'patients':
+			if (validated) {
+				document.getElementById("patientNav").style.display = "block";
+			} else {
+				document.getElementById("validatePat").style.display = "block";
+			}
+			break;
+	}
 
 	// Get all elements with class="content" and hide them
-	tabcontent = document.getElementsByClassName("content");
+	panecontent = document.getElementsByClassName("content");
+	for (i = 0; i < panecontent.length; i++) {
+		panecontent[i].style.display = "none";
+	}
+
+	document.getElementById(paneName).style.display = "block";
+};
+
+function openTab(tabName) {
+
+	// Get all elements with class="tab" and hide them
+	tabcontent = document.getElementsByClassName("tab");
 	for (i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
 
 	document.getElementById(tabName).style.display = "block";
 
-  if (tabName === "main") {
-    document.getElementById("backButton").style.display = "none";
-  } else {
-    document.getElementById("backButton").style.display = "block";
-  }
+};
+
+function validate(paneName) {
+	cookieString = document.cookie;
+	switch(paneName) {
+		case 'receptionists':
+			if (cookieString.includes("receptionist=true")) {
+				return true;
+			}
+		case 'dentists':
+			if (cookieString.includes("dentist=true")) {
+				return true;
+			}
+		case 'patients':
+			if (cookieString.includes("patient=true")) {
+				return true;
+			}
+	}
+	return false;
+
+};
+
+function validateReceptionist() {
+	//todo connect to the DB, determine if the user has permissions
+	document.cookie = "receptionist=true; max-age=3600";
+	cookieString = document.cookie;
+	console.log(cookieString);
+	openPane("receptionists");
+};
+
+function validateDentist() {
+	//todo connect to the DB, determine if the user has permissions
+	document.cookie = "dentist=true; max-age=3600";
+	openPane("dentists");
+};
+
+function validatePatient() {
+	//todo connect to the DB, determine if the user has permissions
+	document.cookie = "patient=true; max-age=3600";
+	openPane("patients");
+};
+
+function createPatient() {
+	//todo connect to the DB, create the Patient and User
+
+};
+
+function patientSearchReception() {
+	//todo connect to the DB, fill in the info
+	document.getElementById("editPatientSearch").style.display = "none";
+	document.getElementById("editPatientForm").style.display = "block";
+};
+
+function updatePatient() {
+	//todo update the patient using connection to the DB
+	document.getElementById("editPatientSearch").style.display = "block";
+	document.getElementById("editPatientForm").style.display = "none";
+};
+
+function cancelUpdatePatient() {
+	document.getElementById("editPatientSearch").style.display = "block";
+	document.getElementById("editPatientForm").style.display = "none";
+};
+
+function branchSearch() {
+	//todo connect to the DB, display all dentists
+
+};
+
+function createEmployee() {
+	//todo connect to the DB, create the Employee and User
+
+};
+
+function patientSearchRecord() {
+	//todo connect to the DB, retrieve the records
+
 };
