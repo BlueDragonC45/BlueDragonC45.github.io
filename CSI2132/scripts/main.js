9ea56dfaf359dbbeed148bc3e2f0216ecf1ab831
BlueDragonC45 <cooldragondjc@gmail.com>
2022-03-18 14:37:59 -0400

    Updates to the site

--- CSI2132/scripts/main.js
+++ CSI2132/scripts/main.js
@@ -1,24 +1,27 @@
 
 function openPane(paneName) {
-	validated = validate(paneName);
+	validated = validate(paneName, true);
 	switch(paneName) {
 		case 'receptionists':
 			if (validated) {
-				document.getElementById("receptionistNav").style.display = "block";
+				document.getElementById("receptionistNav").style.display = "flex";
+				document.getElementById("validateRec").style.display = "none";
 			} else {
 				document.getElementById("validateRec").style.display = "block";
 			}
 			break;
 		case 'dentists':
 			if (validated) {
-				document.getElementById("dentistNav").style.display = "block";
+				document.getElementById("dentistNav").style.display = "flex";
+				document.getElementById("validateDen").style.display = "none";
 			} else {
 				document.getElementById("validateDen").style.display = "block";
 			}
 			break;
 		case 'patients':
 			if (validated) {
-				document.getElementById("patientNav").style.display = "block";
+				document.getElementById("patientNav").style.display = "flex";
+				document.getElementById("validatePat").style.display = "none";
 			} else {
 				document.getElementById("validatePat").style.display = "block";
 			}
@@ -46,19 +49,19 @@ function openTab(tabName) {
 
 };
 
-function validate(paneName) {
+function validate(paneName, testing) {
 	cookieString = document.cookie;
 	switch(paneName) {
 		case 'receptionists':
-			if (cookieString.includes("receptionist=true")) {
+			if (cookieString.includes("receptionist=true") || testing) {
 				return true;
 			}
 		case 'dentists':
-			if (cookieString.includes("dentist=true")) {
+			if (cookieString.includes("dentist=true") || testing) {
 				return true;
 			}
 		case 'patients':
-			if (cookieString.includes("patient=true")) {
+			if (cookieString.includes("patient=true") || testing) {
 				return true;
 			}
 	}
@@ -69,8 +72,6 @@ function validate(paneName) {
 function validateReceptionist() {
 	//todo connect to the DB, determine if the user has permissions
 	document.cookie = "receptionist=true; max-age=3600";
-	cookieString = document.cookie;
-	console.log(cookieString);
 	openPane("receptionists");
 };
 
@@ -81,10 +82,29 @@ function validateDentist() {
 };
 
 function validatePatient() {
-	//todo connect to the DB, determine if the user has permissions
+	//todo connect to the DB, determine if the user has permissions,
+	//find the users name for later use
+	name= ""
 	document.cookie = "patient=true; max-age=3600";
+	document.cookie = "name="+ name +"; max-age=3600";
 	openPane("patients");
 };
+
+function updateDynamicList(divId, list) {
+	listDiv = document.getElementById(divId);
+
+	while (listDiv.hasChildNodes()) {
+		listDiv.removeChild(listDiv.lastChild);
+	}
+
+	for (var i = 0; i < list.length; i++) {
+		var newDiv = document.createElement("div");
+		newDiv.innerHTML = list[i];
+		// newDiv.className += " ";
+		listDiv.appendChild(newDiv);
+	}
+
+}
 
 function createPatient() {
 	//todo connect to the DB, create the Patient and User
