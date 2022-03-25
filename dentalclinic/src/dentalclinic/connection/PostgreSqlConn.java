5a372f60b759c4407b9faf1e3a7060b8af21c59e
dentalpalomo <101893846+dentalpalomo@users.noreply.github.com>
2022-03-25 14:26:34 -0500

    test

--- dentalclinic/src/dentalclinic/connection/PostgreSqlConn.java
+++ dentalclinic/src/dentalclinic/connection/PostgreSqlConn.java
@@ -75,7 +75,7 @@ public class  PostgreSqlConn{
 
 		//@entity can be employee or patient; case-insensitive
 		public boolean isCorrectPwd(String entity, String userName, String Pwd){
-			System.out.println("validating");
+			
 			getConn();
 
 			String foundUser = "";
